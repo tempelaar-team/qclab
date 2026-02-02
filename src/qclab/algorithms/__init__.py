3a2d3ce0eeaa76d9bf1a5efffd67325f6c818295
alexkrotz <alexkrotz@gmail.com>
2026-02-01 21:44:00 -0800

    Added ab initio algorithms.

--- src/qclab/algorithms/__init__.py
+++ src/qclab/algorithms/__init__.py
@@ -2,7 +2,8 @@
 This module imports the algorithm classes to qclab.algorithms.
 """
 
-from qclab.algorithms.mean_field import MeanField
+from qclab.algorithms.mean_field import MeanField, MeanFieldAbInitio
 from qclab.algorithms.fewest_switches_surface_hopping import (
     FewestSwitchesSurfaceHopping,
+    FewestSwitchesSurfaceHoppingAbInitio,
 )
